0a7152b5d5d887f08e26945eec09ad1b5e5f51f7
Espen Bø <espenbo@gmail.com>
2019-07-04 01:18:27 +0200

    First testing started.
    Custom modbus case file. So I could start the frequency converters.
    Reads temperatures from Regin sensors.
    Some customizations in HMI

--- HMI Ventilasjon/lib/filter_1_1-1.0/filter_1_1-1.0.js
+++
@@ -1,4 +0,0 @@
-/*!
-javascript module: filter-1.0.js
-javascript source file path: lib\filter-1.0\filter-1.0.js
-*/
--- HMI Ventilasjon/lib/varmekjolebatteri-1.0/varmekjolebatteri-1.0.js
+++
@@ -1,4 +0,0 @@
-/*!
-javascript module: varmekjolebatteri-1.0.js
-javascript source file path: lib\varmekjolebatteri-1.0\varmekjolebatteri-1.0.js
-*/
